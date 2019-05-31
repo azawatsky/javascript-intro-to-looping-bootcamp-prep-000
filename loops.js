d355feabf5271cc5b84232c46fecc516b9468a5c
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-31 19:58:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -6,6 +6,6 @@ for (let i=0; i<25; i++) {
   if (i<1) {
     console.log(`I am 1 strange loop.`)
   } else {
-    console.log(`I am ${i+1} strange loops.`)
+    console.log(`I am ${i} strange loops.`)
   }
 }
