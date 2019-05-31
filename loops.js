c3f07d47e3c6b8119dcbeae2c36f22288c16d4d2
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-31 19:54:33 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+/*for (var i=1; i<100; i++) {
+  console.log("Hello World the "+ i +" time")
+}*/
+
+for (let i=0; i<25; i++) {
+  if (i=0) {
+    console.log(`I am 1 strange loop.`)
+  } else {
+    console.log(`I am ${i+1} strange loops.`)
+  }
+}
